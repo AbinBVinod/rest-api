090faf058ce683d64a5b175c85aff082b0ae4c6c
Abin <68709126+AbinBVinod@users.noreply.github.com>
2024-02-19 23:10:56 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("hel");
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    Credential: true,
+  }),
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(3000, () => {
+  console.log('server running at http"//localhost:3000/');
+});
